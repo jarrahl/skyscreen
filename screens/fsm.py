000a7ca34bc81c8788a039e6217a6e1ec1665896
Richard Weiss <richardweiss@richardweiss.org>
2015-04-25 00:38:47 +1000

    Game of life - a cool crash

--- screens/fsm.py
+++ screens/fsm.py
@@ -40,59 +40,52 @@ def game_of_life(writer, lock):
 		# gol_arr[:] = np.round(
 		# 	abs(np.random.random((Screen.screen_vane_count, Screen.screen_vane_length)) - 0.3)
 		# )
-		gol_arr[144, 144] = True
-		gol_arr[143, 144] = True
-		gol_arr[142, 144] = True
-		gol_arr[142, 145] = True
-		gol_arr[142, 146] = True
-		gol_arr[143, 147] = True
+		for i in range(0, 100, 5):
+			gol_arr[i+0, i+1] = True
+			gol_arr[i+1, i+2] = True
+			gol_arr[i+2, i+0] = True
+			gol_arr[i+2, i+1] = True
+			gol_arr[i+2, i+2] = True
+
+			gol_arr[i+1+10, i+0] = True
+			gol_arr[i+2+10, i+1] = True
+			gol_arr[i+0+10, i+2] = True
+			gol_arr[i+1+10, i+2] = True
+			gol_arr[i+2+10, i+2] = True
+
+
+			gol_arr[i-1+144, i+0] = True
+			gol_arr[i-2+144, i+1] = True
+			gol_arr[i-0+144, i+2] = True
+			gol_arr[i-1+144, i+2] = True
+			gol_arr[i-2+144, i+2] = True
 
 		while True:
-			lock.frame_ready()
-			print "."
-			writer_buf_reshaped[:, :, 0] = gol_arr * 255
-			writer_buf_reshaped[:, :, 1] = gol_arr * 255
-			writer_buf_reshaped[:, :, 2] = gol_arr * 255
+			right = np.roll(gol_arr, 1)
+			left = np.roll(gol_arr, -1)
+			down = np.roll(gol_arr, 1, axis=0)
+			up = np.roll(gol_arr, -1, axis=0)
+			upright = np.roll(left, 1, axis=0)
+			upleft = np.roll(left, -1, axis=0)
+			downright = np.roll(right, 1, axis=0)
+			downleft = np.roll(right, -1, axis=0)
 
-			#for angle in xrange(1, Screen.screen_vane_count - 1):
-			#	for mag in xrange(1, Screen.screen_vane_length - 1):
-			#		px_sum = 0
-			#		px_sum += (gol_arr[angle, mag] == gol_arr[angle+1, mag  ])
-			#		px_sum += (gol_arr[angle, mag] == gol_arr[angle-1, mag  ])
-			#		px_sum += (gol_arr[angle, mag] == gol_arr[angle+1, mag+1])
-			#		px_sum += (gol_arr[angle, mag] == gol_arr[angle,   mag+1])
-			#		px_sum += (gol_arr[angle, mag] == gol_arr[angle-1, mag+1])
-			#		px_sum += (gol_arr[angle, mag] == gol_arr[angle+1, mag-1])
-			#		px_sum += (gol_arr[angle, mag] == gol_arr[angle,   mag-1])
-			#		px_sum += (gol_arr[angle, mag] == gol_arr[angle-1, mag-1])
-#
-			#		if px_sum == 3:
-			#			gol_arr[angle, mag] = True
-			#		elif px_sum == 4:
-			#			gol_arr[angle, mag] = not gol_arr[angle, mag]
-			#		else:
-			#			gol_arr[angle, mag] = False
-			b = np.roll(gol_arr, 1)
-			t = np.roll(gol_arr, -1)
-			l = np.roll(gol_arr, 1, axis=1)
-			r = np.roll(gol_arr, -1, axis=1)
-			tl = np.roll(t, 1, axis=1)
-			tr = np.roll(t, -1, axis=1)
-			bl = np.roll(b, 1, axis=1)
-			br = np.roll(b, -1, axis=1)
-#
 			px_sums = np.zeros((Screen.screen_vane_count, Screen.screen_vane_length), dtype=np.byte)
-			px_sums += b
-			px_sums += t
-			px_sums += l
-			px_sums += r
-			px_sums += tl
-			px_sums += tr
-			px_sums += bl
-			px_sums += br
+			px_sums += down
+			px_sums += up
+			px_sums += left
+			px_sums += right
+			px_sums += upright
+			px_sums += upleft
+			px_sums += downright
+			px_sums += downleft
+			px_sums += gol_arr
+			#import debug
+
 			gol_arr[px_sums == 3] = True
 			gol_arr[px_sums < 3] = False
 			gol_arr[px_sums > 4] = False
-			gol_arr[px_sums == 4] = np.invert(gol_arr[px_sums == 4])
-
-			time.sleep(0.25)
+			writer_buf_reshaped[:, :, 0] = gol_arr * 255
+			writer_buf_reshaped[:, :, 1] = gol_arr * 255
+			writer_buf_reshaped[:, :, 2] = gol_arr * 255
+			lock.frame_ready()
